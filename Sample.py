784a949df769cee20fc10c03baa04c58aa8d3622
satishlanke <60597522+satishlanke@users.noreply.github.com>
2024-11-11 13:32:16 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,2 +1,6 @@
 To ensure the automation works properly, the data in the Excel sheet named "Loan Position Detail" must be located on a sheet named "R Port Page," and the data table should start from the 6th row. If these conditions are not met, the automation will not function correctly.
 
+
+
+We are going to use four Excel sheets—Loan Position Detail, Non-Cash Collateral, Income Repo, and Performance. Before uploading the data into the tool, please ensure it meets the specified criteria.
+
